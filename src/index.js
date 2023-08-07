3a74e717dbcc6c7a232009b4d21236da70566e14
Hanna0806 <anna.gal0806@gmail.com>
2023-08-07 22:56:48 +0300

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n.toString();
+
+    if (n < 0) {
+        str = str.slice(1);
+    }
+
+    let result = '';
+
+    for (let i = str.length; i > 0; i--) {
+        result = result + str[i - 1];
+    }
+
+    return +result;
 }
